980f1949ac9bd4e324e42392f2339ac2f6928d43
31916088 <258220040@qq.com>
2017-08-18 15:04:17 +0800

    fix

--- app/extend/application.js
+++ app/extend/application.js
@@ -2,6 +2,7 @@ module.exports={
     * spawnExec(spawn){
         yield new Promise(function(resolve,reject){
             spawn.stderr.on('data',function(data){
+                console.log(data);
                 reject(data);
             });
             spawn.on('close',function(data){
