e350a4309cbc4ac9f8d16805749ce85c1d405769
31916088 <258220040@qq.com>
2017-08-18 15:00:44 +0800

    fix

--- app/controller/git.js
+++ app/controller/git.js
@@ -9,12 +9,11 @@ module.exports = app => {
     }
     * commit(){
       var gitDir = this.config.gitDir;
+      var spawn = Spawn('git',['pull'],{cwd:gitDir});
+      yield this.app.spawnExec(spawn);
+      spawn('gitbook',['build'],{cwd:gitDir});
+      yield this.app.spawnExec(spawn);
       this.ctx.body=gitDir;
-      //var spawn = Spawn('git',['pull'],{cwd:gitDir});
-      //yield this.app.spawnExec(spawn);
-      //spawn('gitbook',['build'],{cwd:gitDir});
-      //yield this.app.spawnExec(spawn);
-      //this.ctx.body='sdf';
     }
   }
   return HomeController;
