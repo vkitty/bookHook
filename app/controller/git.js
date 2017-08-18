f18846ed8e43270145ffc2ebe6403b3f8977861f
31916088 <258220040@qq.com>
2017-08-18 15:23:22 +0800

    fix

--- app/controller/git.js
+++ app/controller/git.js
@@ -3,18 +3,20 @@ const path = require('path');
 var Spawn = require('child_process').spawn;
 
 module.exports = app => {
-  class HomeController extends app.Controller {
-    * index() {
-      this.ctx.body = 'hi, egg';
+    class HomeController extends app.Controller {
+        * index() {
+            this.ctx.body = 'hi, egg';
+        }
+
+        * commit() {
+            var gitDir = this.config.gitDir;
+            console.log(gitDir)
+            var spawn = Spawn('git', ['pull'], {cwd: '/home/www/webserver/docs'});
+            yield this.app.spawnExec(spawn);
+            spawn('gitbook', ['build'], {cwd: '/home/www/webserver/docs'});
+            yield this.app.spawnExec(spawn);
+            this.ctx.body = gitDir;
+        }
     }
-    * commit(){
-      var gitDir = this.config.gitDir;
-      var spawn = Spawn('git',['pull'],{cwd:gitDir});
-      yield this.app.spawnExec(spawn);
-      spawn('gitbook',['build'],{cwd:gitDir});
-      yield this.app.spawnExec(spawn);
-      this.ctx.body=gitDir;
-    }
-  }
-  return HomeController;
+    return HomeController;
 };
