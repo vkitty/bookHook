328bf12c2cf3a9652d1215b895484413a8d3ff89
31916088 <258220040@qq.com>
2017-10-09 17:27:01 +0800

    fix

--- app/router.js
+++ app/router.js
@@ -4,4 +4,5 @@ module.exports = app => {
   app.get('/', 'home.index');
   app.post('/git/vkitty', 'git.vkitty');
   app.post('/git/adbaitai', 'git.adbaitai');
+  app.post('/git/frontdocs_adbaitai_com', 'git.frontdocs_adbaitai_com');
 };
