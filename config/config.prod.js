6ac8b8caf177cb26bab107d074a6e27458243910
31916088 <258220040@qq.com>
2017-10-18 13:53:05 +0800

    fix

--- config/config.prod.js
+++ config/config.prod.js
@@ -6,7 +6,7 @@ module.exports = appInfo => {
   // should change to your own
   config.keys = appInfo.name + '_1503034349194_328';
   config.vkittyDir="/home/www/webserver/docs";
-  config.adbaitaiDir="/home/www/webserver/adbaitai/front-docs";
+  config.adbaitaiDir="/home/www/webserver/adbaitai/baitai-docs";
   config.frontdocs_adbaitai_com="/home/admin/docs/front-docs";
   config.frontdocs_adbaitai_com_docs="/home/admin/docs/baitai-docs";
 
